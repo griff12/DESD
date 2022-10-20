9ec98d5577f78f26e1ae101ae645fdf964261c91
Griff <gchristenson3@gatech.edu>
2022-10-20 15:02:11 -0500

    add score footer

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ st.title("Online DESD")
 if "current_word" not in st.session_state:
     st.session_state.counter = 0
     st.session_state.current_word = desd_words[st.session_state.counter]
+    st.session_state.correct = 0
+    st.session_state.wrong = 0
 
 if st.button('Next Word'):
     st.write("Say this word: ")
     st.header(st.session_state.current_word)
+    if speech_result() == st.session_state.current_word:
+        st.session_state.correct += 1 
+    else:
+        st.session_state.wrong += 1
+    score = f"{st.session_state.correct} / {st.session_state.correct + st.session_state.wrong}" 
+    st.write(score)
     st.session_state.counter += 1
     st.session_state.current_word = desd_words[st.session_state.counter]
-    st.write(speech_result())
+    
