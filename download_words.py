17afb9eaa13fd3f854a5a5b810057896a8b487ce
Griffin Christenson <griff1293@gmail.com>
2022-11-01 16:43:57 -0500

    add audio files downloading script

--- download_words.py
+++ download_words.py
@@ -1,31 +1,39 @@
+# %%
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+import requests
 
+# %%
+# do, car, story
 words = [
-            'baby'
-            # , 'one', 'boat', 'do', 'car', 
-            # 'was', 'daddy', 'book', 'good', 'doll', 
-            # 'girl', 'apple', 'they', 'story', 'some',
-            # 'above', 'what', 'any', 'busy', 'night',
-            # 'done', 'huge', 'ocean', 'station', 'could',
-            # 'because', 'echo', 'couple', 'eager', 'together',
-            # 'bought', 'delicious', 'neighbor', 'achieve', 'region',
-            # 'malicious', 'bureau', 'similar', 'campaign', 'waltz',
-            # 'prairie', 'gadget', 'facsimile', 'emphasize', 'prescription',
-            # 'zealous', 'clique', 'atrocious', 'catastrophe', 'liquidate'
+            'baby', 'one', 'boat', 
+            'was', 'daddy', 'book', 'good', 'doll', 
+            'girl', 'apple', 'they', 'some',
+            'above', 'what', 'any', 'busy', 'night',
+            'done', 'huge', 'ocean', 'station', 'could',
+            'because', 'echo', 'couple', 'eager', 'together',
+            'bought', 'delicious', 'neighbor', 'achieve', 'region',
+            'malicious', 'bureau', 'similar', 'campaign', 'waltz',
+            'prairie', 'gadget', 'facsimile', 'emphasize', 'prescription',
+            'zealous', 'clique', 'atrocious', 'catastrophe', 'liquidate'
         ]
-
+# %%
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+# %%
 for word in words:
-    driver.get(f"https://www.google.com/search?q={word}+pronounce")
+    driver.get(f"https://www.google.com/search?q=how+to+pronounce+{word}")
     audio = WebDriverWait(driver, timeout=12).until(lambda x: x.find_element(By.TAG_NAME,"AUDIO"))
-    print(audio.u)
+    src = requests.get(audio.get_attribute('src'))
+    with open(f'audio_files/{word}.mp3', 'wb') as f:
+        f.write(src.content)
+    
 
 
 
 
 
+# %%
