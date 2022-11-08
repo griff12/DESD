3b3a989dddbde92f8fc43085624e82d664d2b068
Griffin Christenson <griff1293@gmail.com>
2022-11-08 15:36:46 -0600

    complete through 9-12

--- decoding_phonetic.py
+++ decoding_phonetic.py
@@ -1,87 +1,91 @@
-# from playsound import playsound
-# import time
 import itertools
 
 sounds = {
-    'ʊ': ['oo', 'u', 'o', 'oul'],
-    'eɪ': ['ai', 'a', 'ay', 'ey', 'ea', 'ei', 'eigh', 'aigh'],
-    'i': ['ee', 'ea', 'ie', 'ei', 'i', 'y', 'e', 'ey'],
-    'ʌ': ['u', 'o', 'ou', 'oe', 'uh'],
-    'oʊ': ['o', 'oa', 'ow', 'oe', 'ough', 'ou'],
-    'u': ['u', 'ui', 'oo', 'ou', 'ue', 'ew', 'o', 'eu', 'oe'],
-    'ɑ': ['o', 'a'],
+    'eɪ': ['a_e', 'ai', 'ay', 'ey', 'ei', 'eigh', 'aigh'],
     'æ': ['a'],
-    'ər': ['er', 'ir', 'or', 'ur', 'ure'],
-    'ð': ['th', 'the'],
+    'i': ['ee', 'ea', 'ie', 'ei', 'i', 'y', 'e', 'ey'],
     'ɛ': ['e'],
-    'ɪ': ['i', 'y'],
     'ɑɪ': ['i_e', 'y_e', 'igh', 'ie'],
+    'ɪ': ['i', 'y', 'e', 'ih'],
+    'oʊ': ['o_e', 'o', 'oa', 'ow', 'oe', 'ough', 'ou', 'oh'],
+    'ɑ': ['o', 'a'],
     'yu': ['u_e', 'ue', 'ew'],
+    'ʌ': ['u', 'uh'],
+    'ʊ': ['oo', 'u', 'ou'],
+    'u': ['u', 'ui', 'oo', 'ou', 'ue', 'ew', 'eu'],
+    'ər': ['r', 'er', 'ir', 'or', 'ur', 'ure'],
+    'ð': ['th', 'the'],
+    'ɔ': ['o', 'augh', 'ough', 'aw', 'a', 'au'], 
+    # '*ə': ['*'],
+    'ə': ['a', 'e', 'i', 'o', 'u', 'ah', 'eh', 'ih', 'uh'],
     'dʒ': ['j'],
-    'k': ['k', 'c', 'ch', 'ck', 'cc'],
-    'l': ['l', 'll', 'le'],
-    'z': ['z', 'zz', 's'],
-    'p': ['p', 'pe', 'pp'],
-    'r': ['r', 'rr', 're'],
-    'n': ['n', 'nn']
-    # 'g': ['g', 'gg'],
-    # 'd': ['d', 'dd']
+    'ʃ': ['sh', 'ch', 'ti', 'ss', 's'],
+    'tʃ' : ['ch'],
+    'y': ['y', 'i'],
+    'w': ['w', 'wh', 'u'],
+    # '*y': ['#'],
+    'k': ['k', 'c', 'ck'],
+    'z': ['z', 's'],
 }
 
 phonetic_desd_words = {
-    'baby': ['b', 'eɪ', 'b', 'i'], 'one': ['w', 'ʌ', 'n'], 'boat': ['b', 'oʊ', 't'], 'do': ['d', 'u'], 'car': ['k', 'ɑ', 'r'],
-    'was': ['w', 'ʌ', 'z'], 'daddy': ['d', 'æ', 'd', 'i'], 'book': ['b', 'ʌ', 'k'], 'good': ['g', 'ʌ', 'd'], 'doll': ['d', 'ɑ', 'l'],
-    'girl': ['g', 'ər', 'l'], 'apple': ['æ', 'p', 'ə', 'l'], 'they': ['ð', 'eɪ'], 'story': ['s', 't', 'oʊ', 'r', 'i'], 'some': ['s', 'ʌ', 'm'],
-    'above': ['ʌ', 'b', 'ʌ', 'v'], 'what': ['w', 'ʌ', 't'], 'any': ['ɛ', 'n', 'i'], 'busy': ['b', 'ɪ', 'z', 'i'], 'night': ['n', 'ɑɪ', 't'],
-    'done': ['d', 'ʌ', 'n'], 'huge': ['h', 'yu', 'dʒ'], 'ocean': None, 'station': None, 'could': None
+    'baby': [['b','eɪ','b','i']], 'one': [['w','ʌ','n']], 'boat': [['b','oʊ','t']], 'do': [['d','u']], 'car': [['k','ɑ','r']],
+    'was': [['w','ʌ','z']], 'daddy': [['d','æ','d','i']], 'book': [['b','ʌ','k']], 'good': [['g','ʌ','d']], 'doll': [['d','ɑ','l']],
+    'girl': [['g','ər','l']], 'apple': [['æ','p','ə','l'], ['æ','p','l']], 'they': [['ð','eɪ']], 'story': [['s','t','oʊ','r','i']], 'some': [['s','ʌ','m']],
+    'above':[['ʌ','b','ʌ','v']], 'what': [['w','ʌ','t']], 'any': [['ɛ','n','i']], 'busy': [['b','ɪ','z','i']], 'night': [['n','ɑɪ','t']],
+    'done': [['d','ʌ','n']], 'huge': [['h','yu','dʒ']], 'ocean': [['oʊ','ʃ','ə','n']], 'station': [['s','t','eɪ','ʃ','ə','n']], 'could': [['k','ʊ','d']],
+    'because': [['b','ɪ','k','ʌ','z'],['b','ʌ','k','ʌ','z']], 'echo': [['ɛ','k','oʊ']], 'couple': [['k','ʌ','p','*ə','l']], 'eager': [['i','g','ər']], 'together': [['t','ə','g','ɛ','ð','ər']],
+    'bought': [['b','ɔ','t']], 'delicious': [['d','ɪ','l','ɪ','ʃ','ə','s']], 'neighbor': [['n','eɪ','b','ər']], 'achieve': [['ə','tʃ','i','v']], 'region': [['r','i','dʒ','ə','n']],
+    'malicious': [['m','ə','l','ɪ','ʃ','ə','s']], 'bureau': [['b','y','ʊ','ər','oʊ']], 'similar': [['s','ɪ','m','ə','l','ər']], 'campaign': [['k','æ','m','p','eɪ','n']], 'waltz': [['w','ɔ','l','t','s']],
+    'prairie': [['p','r','ɛ','ər','i' ], ['p','r','eɪ','ər','i']], 'gadget': [['g','æ','dʒ','ɪ','t']], 'facsimile': [['f','æ','k','s','ɪ','m','ə','l','i']], 'emphasize': [['ɛ','m','f','ə','s','ɑɪ','z']], 'prescription': [['p','r','ɪ','s','k','r','ɪ','p','ʃ','ə','n']],
+    'zealous': [['z','ɛ','l','ə','s']], 'clique': [['k','l','i','k']], 'atrocious': [['ə','t','r','oʊ','ʃ','ə','s']], 'catastrophe': [['k','ə','t','æ','s','t','r','ə','f','i']], 'liquidate': [['l','ɪ','k','w','ɪ','d','eɪ','t']]
 }
 
 def correct(test_word, test_answer):
-    possible_answers = {}
-    for i, sound in enumerate(phonetic_desd_words[test_word]):
-        if sound in sounds:
-            possible_answers[i] = sounds[sound]
-        else:
-            possible_answers[i] = sound
-    
-    combos = []    
-    for sound in possible_answers.values():
-        if type(sound) == list:
-            combos.append(sound)
-    combos = list(itertools.product(*combos))
-    
-    template = []
-    i = 0
-    for sound in possible_answers.values():
-        if type(sound) == list:
-            template.append(str(i))
-            i += 1
-        else:
-            template.append(sound)
-    
-    correct_answers = []
-    for combo in combos:
-        answer = ''
-        for sound in template:
-            if sound.isalpha():
-                answer = answer + sound
+    correct_answers = [] # Answers that will be accepted at the end
+    for pronounciation in phonetic_desd_words[test_word]:
+        possible_answers = {}
+        for x, sound in enumerate(pronounciation):
+            if sound in sounds:
+                possible_answers[x] = sounds[sound]
             else:
-                answer = answer + combo[int(sound)]
-        if '_e' in answer:
-            answer = answer.replace('_e', '') + 'e'
-            correct_answers.append(answer[:-1])
-        correct_answers.append(answer)
+                possible_answers[x] = sound
+
+        combos = [] # Initial list of sounds that have multiple spellings   
+        for sound in possible_answers.values():
+            if type(sound) == list:
+                combos.append(sound)
+        combos = list(itertools.product(*combos)) # List of tuples with all possible combinations for sounds with multiple spellings
+
+        # Number the places within the word which have multiple possible spellings
+        template = []
+        x = 0
+        for sound in possible_answers.values():
+            if type(sound) == list:
+                template.append(str(x))
+                x += 1
+            else:
+                template.append(sound)
+
+        # Insert spelling possibilities at correct locations
+        for combo in combos:
+            answer = ''
+            for sound in template:
+                if sound.isalpha():
+                    answer = answer + sound
+                else:
+                    answer = answer + combo[int(sound)]
+            # Account for 'long' vowels due to ending in 'e'
+            if '_e' in answer:
+                answer = answer.replace('_e', '') + 'e'
+                # Include assumption for long vowel, for instace 'nit' for night
+                correct_answers.append(answer[:-1])
+            correct_answers.append(answer)
     print(correct_answers)
     return test_answer in correct_answers
 
-# def run(test_word, test_answer): 
-#     playsound(f'audio_files/{test_word}.mp3')
-#     time.sleep(2)
-#     playsound(f'audio_files/{test_word}.mp3')
-#     return correct(test_word, test_answer)
-
 if __name__ == '__main__':
-    print(correct('any', 'huj'))
+    print(correct('liquidate', 'lickwidate'))
 # playsound('audio_files/baby.mp3')
 # time.sleep(2)
 # playsound('audio_files/baby.mp3')
